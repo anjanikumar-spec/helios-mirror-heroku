c065b12f955b08958115dcf6f104af9c5d3921a2
Anjan Budige Altruist <61613373+anjan-budige@users.noreply.github.com>
2022-11-12 08:52:44 +0530

    Update maruf.sh

--- maruf.sh
+++ maruf.sh
@@ -1,5 +1,5 @@
 apt install git -y
-git clone https://github.com/anjanikumar-spec/helios-mirror
+git clone https://github.com/anjan-budige/helios-mirror
 cd helios*
 pip3 install --no-cache-dir -r requirements.txt
 bash start.sh
